f7d51fd0a0313f6c3adfd3f5006a90e54fe4636c
mmalikahmed <malik.munawar@live.com>
2018-11-12 03:00:44 -0500

    Update CodeChef/Program.cs
    Codechef ATM solution

---
+++ CodeChef/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Codechef_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            input = Console.ReadLine();
+
+            string[]    arr      = input.Split(' ');
+            int         withdraw = int.Parse(arr[0]);
+            double      total    = double.Parse(arr[1]);
+
+            if (withdraw % 5 == 0 &&
+                withdraw + 0.5 <= total)
+            {
+                total = total - withdraw - 0.5;
+                Console.WriteLine(string.Format("{0:0.00}", total));
+            }
+            else
+            {
+                Console.WriteLine(total);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
